292fcaa055469e3c5fca7bb798e8639ad75e496f
Stive Lamatta <167715244+StiveLamatta@users.noreply.github.com>
2025-05-26 11:14:14 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"mysapp/static"), os.path.join(BASE_DIR,"educacion/static")]
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = 'static/'
 
 # Default primary key field type
